49290a5ba3fa33b81a0cb05ceb6e64f8efa2344b
Bruno Rodrigues <rbruno216@gmail.com>
2023-04-23 11:56:58 -0300

    Vídeo: como criar o efeito parllax com ondas

---
+++ 10 - paralax com ondas/main.js
@@ -0,0 +1,14 @@
+
+var onda1 = document.getElementById('onda1')
+var onda2 = document.getElementById('onda2')
+var onda3 = document.getElementById('onda3')
+var onda4 = document.getElementById('onda4')
+
+window.addEventListener('scroll', function(){
+    var rolagemPos = window.scrollY
+
+    onda1.style.backgroundPositionX = 400 + rolagemPos * 4 + 'px';
+    onda2.style.backgroundPositionX = 300 + rolagemPos * -4 + 'px';
+    onda3.style.backgroundPositionX = 200 + rolagemPos * 2 + 'px';
+    onda4.style.backgroundPositionX = 100 + rolagemPos * -2 + 'px';
+})
