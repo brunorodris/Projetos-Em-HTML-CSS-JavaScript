1ba5bc190f14d5961ddf67261fcb493cf705f5e4
Bruno Rodrigues <rbruno216@gmail.com>
2023-08-28 16:54:16 -0300

    Projeto do portfólio personalizado
    Projeto realizado no canal Inteliogia.

---
+++ 18 - portfólio personalizado/menu.js
@@ -0,0 +1,17 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
